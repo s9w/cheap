54103058f7cb0597bf7e7a93a3e403099de55c45
Sebastian Werhausen <swerhausen@gmail.com>
2022-10-25 00:00:00 +0000

    tests

--- cheap/main.cpp
+++ cheap/main.cpp
@@ -1,4 +1,4 @@
-// #include <fstream>
+#include <fstream>
 
 #define FMT_HEADER_ONLY
 #include <fmt/format.h>
@@ -14,52 +14,36 @@
 
 using namespace cheap;
 
-TEST_CASE("attributes") {
+TEST_CASE("attributes basics"){
    CHECK(std::holds_alternative<bool_attribute>("xxx"_att));
    CHECK(std::holds_alternative<string_attribute>("xxx=yyy"_att));
-   CHECK_THROWS_AS("hidden=xxx"_att, cheap_exception);
-   CHECK_THROWS_AS("autocapitalize=xxx"_att, cheap_exception);
-
-   // auto elem = create_element("div", "cool=true"_att, "hallo");
-   auto elem = div("data-cool=true"_att,
-      span("first"),
-      span("second")
-      );
-
-   
-   // element elem{ "div", {"cool=true"_att},
-   //    {
-   //       element{"span", {"first"}},
-   //       element{"span", {"second"}}
-   //    }
-   // };
-
-   // element x{ "span", "hey" };
-   // element elem{ "div", {"cool=true"_att}, element{"span", "hey"} };
-
-   // auto elem = div({ "some string content" });
-   std::ofstream file_out("test.html");
-   file_out << get_element_str(elem);
 }
 
+TEST_CASE("attributes error checks") {
+   CHECK_THROWS_AS("hidden=xxx"_att, cheap_exception);
+   CHECK_THROWS_AS("autocapitalize=xxx"_att, cheap_exception);
+   CHECK_THROWS_AS("id"_att, cheap_exception);
+}
 
-// int main()
-// {
-//    using namespace cheap;
-//
-//    const auto att0 = "x=y"_att;
-//    const auto att1 = "x"_batt;
-//    
-//    arbitrary_element sub{ .m_type = "span", .m_inner_html = {"content0"} };
-//    span s0{ .m_inner_html = {""}};
-//    cheap::div main{
-//       .m_attributes= {"hidden=b"_att, "doit"_batt},
-//       .m_inner_html = {"content0", sub, s0}
-//    };
-//    const auto str = get_element_str(main);
-//
-//    std::ofstream file_out("test.html");
-//    file_out << str;
-//
-//    return 0;
-// }
+TEST_CASE("get_element_str()") {
+   CHECK_EQ(get_element_str(div()), "<div></div>\n");
+   CHECK_EQ(get_element_str(div("bool"_att)), "<div bool></div>\n");
+   CHECK_EQ(get_element_str(div("key=val"_att)), "<div key=\"val\"></div>\n");
+   CHECK_EQ(get_element_str(div("abc")), "<div>abc</div>\n");
+   CHECK_EQ(get_element_str(div(span("abc"))), "<div>\n    <span>abc</span>\n</div>\n");
+   CHECK_EQ(get_element_str({div(), div()}), "<div></div>\n<div></div>\n");
+   CHECK_EQ(get_element_str(img()), "<img />\n");
+}
+
+TEST_CASE("playground"){
+   // const std::string elem_str = get_element_str(
+   //    {
+   //       div(img("src=test.jpg"_att))
+   //       // , div(img("src=test.jpg"_att))
+   //    }
+   // );
+   const std::string elem_str = get_element_str(div(span("first"), img("src=test.jpg"_att)));
+   std::ofstream file_out("test.html");
+   // file_out << get_element_str(elem);
+   file_out << elem_str;
+}
